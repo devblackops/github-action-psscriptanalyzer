5b82a94890dc8424cb6f6d47a902e2d2f8f6186d
Brandon Olin <brandon@devblackops.io>
2018-12-28 22:12:29 -0800

    add psake tasks

---
+++ analyze/psakeFile.ps1
@@ -0,0 +1,32 @@
+task default -depends Build
+
+task Init {
+    $script:dockerAcct = 'devblackops'
+    $script:imageName  = $env:BHProjectName
+    $script:dockerRepo = "$dockerAcct/$imageName"
+    $script:version    = '1.0.0'
+}
+
+task Build -depends Init {
+    "Building [$tag]"
+    exec {
+        docker build -t "$dockerRepo`:$version" .
+    }
+}
+
+task Login {
+    exec {
+        docker login
+    }
+}
+
+task Push -depends Build, Login {
+    exec {
+        docker push "$dockerRepo`:$version"
+    }
+}
+
+task ? {
+    'Available tasks:'
+    $psake.context.Peek().Tasks.Keys | Sort-Object
+}
---
+++ build.ps1
@@ -0,0 +1,34 @@
+[cmdletbinding(DefaultParameterSetName = 'task')]
+param(
+    [parameter(ParameterSetName = 'task', Position = 0)]
+    [string[]]$Task = 'default',
+
+    [parameter(ParameterSetName = 'help')]
+    [switch]$Help,
+
+    [switch]$Bootstrap
+)
+
+$ErrorActionPreference = 'Stop'
+
+# Bootstrap dependencies
+if ($Bootstrap.IsPresent) {
+    Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+        Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser
+    }
+    Import-Module -Name PSDepend -Verbose:$false
+    Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
+}
+
+# Execute psake task(s)
+$psakeFile = './analyze/psakeFile.ps1'
+Set-BuildEnvironment -Force -WarningAction SilentlyContinue
+if ($PSCmdlet.ParameterSetName -eq 'Help') {
+    Get-PSakeScriptTasks -buildFile $psakeFile  |
+        Format-Table -Property Name, Description, Alias, DependsOn
+} else {
+    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -Verbose:($VerbosePreference -eq 'Continue')
+    exit ([int](-not $psake.build_success))
+}
